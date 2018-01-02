868f9330026d1b9ced1feb614eb48c42c93f3445
swapnil singh <swappp7singh@gmail.com>
2018-01-02 21:35:30 +0530

    included switch module

---
+++ Switch/src/in/swapnilsingh/Main.java
@@ -0,0 +1,63 @@
+package in.swapnilsingh;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int switchValue = 5;
+//
+//        switch (switchValue) {
+//            case 1:
+//                System.out.println("Value was 1");
+//                break;
+//            case 2:
+//                System.out.println("Value was 2");
+//                break;
+//            case 3:
+//                System.out.println("Value was 3");
+//                break;
+//            default:
+//                System.out.println("was not 1 or 2 or 3");
+//                break;
+//        }
+
+//        char newVar = 'E';
+
+//        switch (newVar) {
+//            case 'A':
+//                System.out.println("Value name is A");
+//                break;
+//            case 'B':
+//                System.out.println("Value name is B");
+//                break;
+//            case 'C':
+//                System.out.println("Value name is C");
+//                break;
+//            case 'D': case 'E': case 'F':
+//                System.out.println("Value is between D, E or F");
+//                System.out.println("Value was " + newVar);
+//                break;
+//            default:
+//                System.out.println("Value didn't match any case.");
+//                System.out.println("Actual value was " + newVar);
+//                break;
+//        }
+
+        String month = "June";
+
+        switch (month) {
+            case "January":
+                System.out.println("Month was january");
+                break;
+            case "February":
+                System.out.println("Month was february");
+                break;
+            case "March":
+                System.out.println("Month was march");
+                break;
+            default:
+                System.out.println("Month was not found");
+                System.out.println("The value of month was " + month);
+        }
+
+    }
+}
